9748834456dc77dcec2ea01d8c28a647181b719c
Fábio Moraes Rosa <fabiomrosa@gmail.com>
2022-03-10 18:24:19 -0300

    update demo code for readability

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,1 +1,134 @@
-function e(){var e,l;e='<div id="linguagens" class="list-picker">',e+='  <ul class="list-picker-list is-options-list"></ul>';for(var r=0;r<c;r++)e+='  <ul class="list-picker-list is-picks-list"></ul>';e+="</div>",document.querySelector(".data").style.display="none",document.querySelector("#example").innerHTML=e,l={list:["ActionScript","CSS","C#","HTML","Java","JavaScript","Python","Ruby","Swift"],gap:1,drag:s,fixed:t,order:i,template:function(e,s){var t='<div class="card">';return t+=' <div class="card-close">',t+='   <span class="card-close-icon card-close-plus">&plus;</span>',t+='   <span class="card-close-icon card-close-times">&times;</span>',t+=" </div>",t+=' <div class="card-content">',t+='   <p class="card-content-text">'+s+"</p>",t+=" </div>",t+="</div>"}},d=new ListPicker("#linguagens",l)}var s=!1,t=!1,c=3,i=!0,d=null,l=document.querySelector("#select"),r=document.querySelector("#drag"),a=document.querySelector("#order"),n=document.querySelector("#keep"),o=document.querySelector("#loose"),u=document.querySelector("#fixed"),v=document.querySelector("#remove"),m=document.querySelector("#add"),L=document.querySelector("#return");l.addEventListener("click",function(c){t||(s=!1,l.classList.add("is-selected"),r.classList.remove("is-selected"),e())}),r.addEventListener("click",function(c){t||(s=!0,l.classList.remove("is-selected"),r.classList.add("is-selected"),e())}),a.addEventListener("click",function(s){t||(i=!0,a.classList.add("is-selected"),n.classList.remove("is-selected"),e())}),n.addEventListener("click",function(s){t||(i=!1,a.classList.remove("is-selected"),n.classList.add("is-selected"),e())}),o.addEventListener("click",function(s){t=!1,o.classList.add("is-selected"),u.classList.remove("is-selected"),l.removeAttribute("disabled"),r.removeAttribute("disabled"),a.removeAttribute("disabled"),n.removeAttribute("disabled"),e()}),u.addEventListener("click",function(c){s=!1,t=!0,l.classList.add("is-selected"),r.classList.remove("is-selected"),o.classList.remove("is-selected"),u.classList.add("is-selected"),l.setAttribute("disabled","disabled"),r.setAttribute("disabled","disabled"),a.setAttribute("disabled","disabled"),n.setAttribute("disabled","disabled"),e()}),v.addEventListener("click",function(s){c>1&&c--,e()}),m.addEventListener("click",function(s){c<3&&c++,e()}),L.addEventListener("click",function(e){for(var s=document.querySelector(".data"),t=s.querySelector("code"),c=d.getPicks(),i="[\n",l=0;l<c.length;l++)i+="  "+l+": ["+c[l]+"]\n";i+="]",s.style.display="block",t.textContent=i}),e();
+var drag = false;
+var fixed = false;
+var lists = 3;
+var order = true;
+var picker = null;
+
+var _select = document.querySelector('#select');
+var _drag = document.querySelector('#drag');
+var _order = document.querySelector('#order');
+var _keep = document.querySelector('#keep');
+var _loose = document.querySelector('#loose');
+var _fixed = document.querySelector('#fixed');
+var _remove = document.querySelector('#remove');
+var _add = document.querySelector('#add');
+var _return = document.querySelector('#return');
+
+_select.addEventListener('click', function(e) {
+  if (fixed) return;
+  drag = false;
+  _select.classList.add('is-selected');
+  _drag.classList.remove('is-selected');
+  listPicker();
+});
+
+_drag.addEventListener('click', function(e) {
+  if (fixed) return;
+  drag = true;
+  _select.classList.remove('is-selected');
+  _drag.classList.add('is-selected');
+  listPicker();
+});
+
+_order.addEventListener('click', function(e) {
+  if (fixed) return;
+  order = true;
+  _order.classList.add('is-selected');
+  _keep.classList.remove('is-selected');
+  listPicker();
+});
+
+_keep.addEventListener('click', function(e) {
+  if (fixed) return;
+  order = false;
+  _order.classList.remove('is-selected');
+  _keep.classList.add('is-selected');
+  listPicker();
+});
+
+_loose.addEventListener('click', function(e) {
+  fixed = false;
+  _loose.classList.add('is-selected');
+  _fixed.classList.remove('is-selected');
+  _select.removeAttribute('disabled');
+  _drag.removeAttribute('disabled');
+  _order.removeAttribute('disabled');
+  _keep.removeAttribute('disabled');
+  listPicker();
+});
+
+_fixed.addEventListener('click', function(e) {
+  drag = false;
+  fixed = true;
+  _select.classList.add('is-selected');
+  _drag.classList.remove('is-selected');
+  _loose.classList.remove('is-selected');
+  _fixed.classList.add('is-selected');
+  _select.setAttribute('disabled', 'disabled');
+  _drag.setAttribute('disabled', 'disabled');
+  _order.setAttribute('disabled', 'disabled');
+  _keep.setAttribute('disabled', 'disabled');
+  listPicker();
+});
+
+_remove.addEventListener('click', function(e) {
+  if (lists > 1) lists--;
+  listPicker();
+});
+
+_add.addEventListener('click', function(e) {
+  if (lists < 3) lists++;
+  listPicker();
+});
+
+_return.addEventListener('click', function(e) {
+  var data = document.querySelector('.data');
+  var code = data.querySelector('code');
+  var picks = picker.getPicks();
+
+  var array = `
+    [\n
+      ${picks.forEach((pick, i) => `'${i}': ['${pick}']`)}
+    \n]
+  `;
+
+  data.style.display = 'block';
+  code.textContent = array;
+});
+
+function listPicker() {
+  var boilerplate = `
+    <div id="linguagens" class="list-picker">
+      <ul class="list-picker-list is-options-list"></ul>
+      ${lists.forEach(() => '<ul class="list-picker-list is-picks-list"></ul>')}
+    </div>
+  `;
+
+  var settings = {
+    list: ['TypeScript', 'CSS', 'C#', 'HTML', 'Java', 'JavaScript', 'Python', 'Ruby', 'Swift'],
+    gap: 1,
+    drag: drag,
+    fixed: fixed,
+    order: order,
+    template: function(index, value) {
+      return `
+        <div class="card">
+          <div class="card-close">
+            <span class="card-close-icon card-close-plus">&plus;</span>
+            <span class="card-close-icon card-close-times">&times;</span>
+          </div>
+          <div class="card-content">
+            <p class="card-content-text">${value}</p>
+          </div>
+        </div>
+      `;
+    }
+  }
+
+  document.querySelector('.data').style.display = 'none';
+  document.querySelector('#example').innerHTML = boilerplate;
+
+  picker = new ListPicker('#linguagens', settings);
+}
+
+listPicker();
